1a3f8b6f0b7ab6a28bf5146bf3b854ffd8eed9eb
Naoto Ikeno <ikenox@gmail.com>
2017-07-10 15:56:03 +0900

    update

--- custom.js
+++ custom.js
@@ -1,12 +1,7 @@
-Jupyter.notebook.config.update({
-  'load_extensions': { 'vim_binding': true },
-});
-
 // Configure CodeMirror Keymap
 require([
   'nbextensions/vim_binding/vim_binding',   // depends your installation
 ], function() {
-  console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> custom.js');
   CodeMirror.Vim.map("L", "$", "normal");
   CodeMirror.Vim.map("L", "$", "visual");
   CodeMirror.Vim.map("H", "^", "normal");
@@ -22,9 +17,6 @@ require([
   CodeMirror.Vim.map("k", "<Plug>(vim-binding-gk)", "normal");
   CodeMirror.Vim.map("gj", "<Plug>(vim-binding-j)", "normal");
   CodeMirror.Vim.map("gk", "<Plug>(vim-binding-k)", "normal");
-
-  CodeMirror.Vim.map("x", "\"_x", "normal");
-  CodeMirror.Vim.map("x", "\"_x", "visual");
 
   CodeMirror.Vim.map("==", "gg=G", "normal");
 });
