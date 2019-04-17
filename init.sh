2e5c814b5290f828911c502529e7311d1155fcdb
Naoto Ikeno <naoto.ikeno@dena.com>
2019-04-17 18:23:12 +0900

    update

--- init.sh
+++ init.sh
@@ -3,6 +3,8 @@ if [ $? -ne 0 ];
 then
   echo "install homebrew"
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+else
+  echo "homebrew is already installed."
 fi
 
 which ansible >/dev/null 2>&1
@@ -10,4 +12,6 @@ if [ $? -ne 0 ];
 then
   echo "install ansible"
   brew install ansible
+else
+  echo "ansible is already installed."
 fi
