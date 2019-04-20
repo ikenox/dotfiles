6137b696dde102c8ee13bb4720bf3850b1776f97
Naoto Ikeno <naoto.ikeno@dena.com>
2019-04-20 15:17:23 +0900

    update

--- provision.rb
+++ provision.rb
@@ -4,26 +4,55 @@
 # todo dry-run mode
 
 def run
-  task :install_homebrew, do_if: has_err("which brew") do
+  task 'install homebrew', do_if: has_err("which brew") do
     sh '/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"'
   end
-  task :install_git, do_if: has_err("which git") do
+  task 'install brew cask', do_if: has_err("brew cask") do
+    sh 'brew tap caskroom/cask'
+  end
+  task 'install git', do_if: has_err("which git") do
     sh "brew install git"
   end
-  task :install_ghq, do_if: has_err("which ghq") do
-    sh "brew install ghq"
-  end
-
-  task :put_dotfiles_repo, do_if: not_exist("~/.dotfiles") do
+  task 'put dotfiles repo', do_if: not_exist("~/.dotfiles") do
     # todo read ghq root dir from gitconfig
-    task :git_clone_dotfiles, do_if: not_exist("~/repo/github.com/ikenox/dotfiles") do
+    task 'clone dotfiles repo', do_if: not_exist("~/repo/github.com/ikenox/dotfiles") do
       sh "git clone git@github.com:ikenox/dotfiles.git ~/repo/github.com/ikenox/dotfiles"
     end
-    task :symlink_dotfiles_dir do
-      sh "ln -s ~/.dotfiles"
+    task 'symlink dotfiles' do
+      sh "ln -si ~/.dotfiles"
     end
   end
+  task 'put gitconfig', do_if: has_err("ls -la ~/.gitconfig | grep '-> ~/.dotfiles/git/gitconfig'") do
+    sh 'ln -si ~/.gitconfig ~/.dotfiles/git/gitconfig'
+  end
 
+  end
+
+  ['peco', 'fzf', 'mas', 'jq', 'ghq'].each do |p|
+    task "install_#{p}", do_if: has_err("which #{p}") do sh "brew install #{p}"
+  end
+
+  ['slack', 'karabiner-elements', 'alfred', 'iterm2', 'caffeine', 'google-japanese-ime'].each do |p|
+    task "install_#{p}", do_if: has_err("which #{p}") do sh "brew cask install #{p}"
+  end
+
+  # brew cask
+      InstallHomeBrewCaskTask(),
+      BrewCaskTask('slack'),
+      BrewCaskTask('karabiner-elements'),
+      BrewCaskTask('alfred'),
+      BrewCaskTask('iterm2'),
+      BrewCaskTask('caffeine'),
+      BrewCaskTask('google-japanese-ime'),
+
+  # mas
+      MasTask(409183694),  # keynote
+      MasTask(409203825),  # Numbers
+      MasTask(409201541),  # Pages
+      MasTask(539883307),  # LINE
+      MasTask(409183694),  # Keynote
+      MasTask(485812721),  # TweetDeck
+      MasTask(405399194),  # Kindle
   task :hoge do
     task :fuga do
 
@@ -32,44 +61,6 @@ def run
 
     end
   end
-
-  # task :git, do_if: -> {
-  #   has_err("which git")
-  # } do
-  #   puts "helloooo"
-  # end
-
-  # task :install_homebrew, do_if:has_err("which brew") {
-  # }
-  #
-  # task :hoge, {}
-  #
-  # task :init do
-  #   task :clone_dotfiles_repo, do_if:(1) {  }
-  # end
-  # task :git, do_if: has_err("which git") {sh "brew install git"}
-  # task :ghq, do_if: has_err("which git") {sh "brew install ghq"}
-  #
-  # task :tap_brew_cask, do_if: has_err("brew cask") {sh "brew tap caskroom/cask"}
-  #
-  # task :peco, do_if: has_err("which peco") {sh "brew install peco"}
-  # task :fzf, do_if: has_err("which peco") {sh "brew install peco"}
-
-
-  # task_if has_err("/usr/local/Caskroom/slack") ->{}
-  #
-  # # brew
-  # task_brew 'peco'
-  # task_brew 'mas'
-  # task_brew 'fzf'
-  #
-  # # cask
-  # task_brew_cask('slack'),
-  #     task_brew_cask('karabiner-elements'),
-  #     task_brew_cask('alfred'),
-  #     task_brew_cask('iterm2'),
-  #     task_brew_cask('caffeine'),
-  #     task_brew_cask('google-japanese-ime'),
 
 end
 
@@ -104,6 +95,10 @@ end
 def sh(cmd)
   system cmd, exception: true
   nil
+end
+
+def
+
 end
 
 run
