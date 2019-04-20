df21603f4e27688d89abd8ae8c07e6e165b33848
Naoto Ikeno <naoto.ikeno@dena.com>
2019-04-20 18:03:20 +0900

    update

--- provision.rb
+++ provision.rb
@@ -4,69 +4,53 @@
 # todo dry-run mode
 
 def run
-  task 'install homebrew', do_if: has_err("which brew") do
+  task :install_homebrew, do_if: has_err('which brew') do
     sh '/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"'
   end
-  task 'install brew cask', do_if: has_err("brew cask") do
+  task :tap_brew_cask, do_if: has_err('brew tap | grep caskroom/cask') do
     sh 'brew tap caskroom/cask'
   end
-  task 'install git', do_if: has_err("which git") do
-    sh "brew install git"
+  task :install_git, do_if: has_err('which git') do
+    sh 'brew install git'
   end
-  task 'put dotfiles repo', do_if: not_exist("~/.dotfiles") do
+
+  dotfiles_dir = '~/repo/github.com/ikenox/dotfiles'
+  task :put_dotfiles_repo, do_if: !symlink_exists?(dotfiles_dir, "~/.dotfiles") do
     # todo read ghq root dir from gitconfig
-    task 'clone dotfiles repo', do_if: not_exist("~/repo/github.com/ikenox/dotfiles") do
-      sh "git clone git@github.com:ikenox/dotfiles.git ~/repo/github.com/ikenox/dotfiles"
+    task :clone_dotfiles, do_if: not_exist(dotfiles_dir) do
+      sh "git clone git@github.com:ikenox/dotfiles.git #{dotfiles_dir}"
     end
-    task 'symlink dotfiles' do
-      sh "ln -si ~/.dotfiles"
-    end
-  end
-  task 'put gitconfig', do_if: has_err("ls -la ~/.gitconfig | grep '-> ~/.dotfiles/git/gitconfig'") do
-    sh 'ln -si ~/.gitconfig ~/.dotfiles/git/gitconfig'
+    task_symlink dotfiles_dir, "~/.dotfiles"
   end
 
-  end
+  task_symlink '~/.dotfiles/git/gitconfig', '~/.gitconfig'
 
-  ['peco', 'fzf', 'mas', 'jq', 'ghq'].each do |p|
-    task "install_#{p}", do_if: has_err("which #{p}") do sh "brew install #{p}"
-  end
+  task_brew 'peco'
+  task_brew 'fzf'
+  task_brew 'mas'
+  task_brew 'jq'
+  task_brew 'ghq'
 
-  ['slack', 'karabiner-elements', 'alfred', 'iterm2', 'caffeine', 'google-japanese-ime'].each do |p|
-    task "install_#{p}", do_if: has_err("which #{p}") do sh "brew cask install #{p}"
-  end
+  task_brew_cask 'slack'
+  task_brew_cask 'karabiner-elements'
+  task_brew_cask 'alfred'
+  task_brew_cask 'iterm2'
+  task_brew_cask 'caffeine'
+  task_brew_cask 'google-japanese-ime'
 
-  # brew cask
-      InstallHomeBrewCaskTask(),
-      BrewCaskTask('slack'),
-      BrewCaskTask('karabiner-elements'),
-      BrewCaskTask('alfred'),
-      BrewCaskTask('iterm2'),
-      BrewCaskTask('caffeine'),
-      BrewCaskTask('google-japanese-ime'),
-
-  # mas
-      MasTask(409183694),  # keynote
-      MasTask(409203825),  # Numbers
-      MasTask(409201541),  # Pages
-      MasTask(539883307),  # LINE
-      MasTask(409183694),  # Keynote
-      MasTask(485812721),  # TweetDeck
-      MasTask(405399194),  # Kindle
-  task :hoge do
-    task :fuga do
-
-    end
-    task :foo do
-
-    end
-  end
+  task_mas 409183694 # keynote
+  task_mas 409203825 # Numbers
+  task_mas 409201541 # Pages
+  task_mas 539883307 # LINE
+  task_mas 409183694 # Keynote
+  task_mas 485812721 # TweetDeck
+  task_mas 405399194 # Kindle
 
 end
 
 $level = -1
 
-def task(name, do_if:true, &block)
+def task(name, do_if: true, &block)
   $level += 1
 
   print "    " * $level
@@ -83,22 +67,54 @@ def task(name, do_if:true, &block)
   $level -= 1
 end
 
-def has_err(cmd)
-  result = system cmd
-  !result
+# task aliases
+def task_symlink(source, symlink_file)
+  task "put_symlink[#{symlink_file} -> #{source}]", do_if: !symlink_exists?(source, symlink_file) do
+    sh "ln -si  #{source} #{symlink_file}"
+  end
 end
 
+def task_brew(p)
+  task "install_#{p}", do_if: not_exist("/usr/local/Cellar/#{p}") do
+    sh "brew install #{p}"
+  end
+end
+
+def task_brew_cask(p)
+  task "install_#{p}", do_if: not_exist("/usr/local/Caskroom/#{p}") do
+    sh "brew cask install #{p}"
+  end
+end
+
+def task_mas(app_id)
+  task "install_app_#{app_id}", do_if: has_err("mas list | grep '^#{app_id} '") do
+    sh "mas install #{app_id}"
+  end
+end
+
+def symlink_exists?(origin_file, symlink_file)
+  current_link = `readlink #{symlink_file}`.chomp
+  (origin_file.start_with? "~", "/") ?
+      File.expand_path(origin_file) == current_link # absolute symlink
+      : origin_file == current_link # relative symlink
+end
+
+def has_err(cmd)
+  puts "> #{cmd}"
+  system cmd
+  $? != 0
+end
+
+system "hofds"
+
 def not_exist(path)
-  has_err("ls #{path}")
+  has_err "ls #{path}"
 end
 
 def sh(cmd)
-  system cmd, exception: true
-  nil
-end
-
-def
-
+  puts "> #{cmd}"
+  system cmd
+  raise "command execution failed" if $? != 0
 end
 
 run
