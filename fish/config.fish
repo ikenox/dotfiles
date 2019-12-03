baf0161de6648d7d06fe40730a3a038b0d6604ad
Naoto Ikeno <naoto.ikeno@dena.com>
2019-12-03 11:45:53 +0900

    update

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,4 @@
-set PATH ~/bin ~/go/bin /usr/local/bin /usr/bin /bin /sbin /usr/sbin $PATH
+set PATH ~/.cargo/bin ~/bin ~/go/bin /usr/local/bin /usr/bin /bin /sbin /usr/sbin $PATH
 set JAVA_HOME (/usr/libexec/java_home -v 11.0.1)
 
 alias c=clear
