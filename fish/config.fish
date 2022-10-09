5fdd85a22d36bc473d253bd0ac25145744c5cdb0
Naoto Ikeno <ikenox@gmail.com>
2022-10-10 01:00:48 +0900

    update

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,4 @@
-set PATH ~/.cargo/bin ~/bin ~/go/bin /usr/local/bin /usr/bin /bin /sbin /usr/sbin /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/bin ~/.local/share/solana/install/active_release/bin/ $PATH
+set PATH ~/.cargo/bin ~/bin ~/go/bin /opt/homebrew/bin /usr/local/bin /usr/bin /bin /sbin /usr/sbin /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/bin ~/.local/share/solana/install/active_release/bin/ $PATH
 export CLOUDSDK_PYTHON=(which python3)
 
 alias c=clear
