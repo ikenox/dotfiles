2e31871563a2e0e697ec630d020bb4f76433a19a
naoto.ikeno <ikenox@gmail.com>
2024-04-17 12:45:47 +0900

    update

--- install.sh
+++ install.sh
@@ -6,6 +6,16 @@ if ! command -v ansible-playbook &> /dev/null
 then
    /usr/bin/python3 -m pip install -U pip
    /usr/bin/python3 -m pip install --user ansible
+else
+  echo "skip: ansible is already installed"
+fi
+
+if ! command -v brew &> /dev/null
+then
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  eval "$(/opt/homebrew/bin/brew shellenv)"
+else
+  echo "skip: homebrew is already installed"
 fi
 
 if [ ! -e "$DOTFILES_DIR" ]; then
@@ -16,10 +26,4 @@ fi
 
 if [ ! -e "$DOTFILES_DIR/vars.yml" ]; then
   echo 'this-is: dummy-value' > $HOME/repos/github.com/ikenox/dotfiles/vars.yml
-fi
-
-if ! command -v brew &> /dev/null
-then
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-  eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
