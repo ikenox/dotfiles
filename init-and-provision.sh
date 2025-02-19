ea8bdedb1a88914aba6dffe488dd3f90c3481219
naoto.ikeno <ikenox@gmail.com>
2025-02-19 14:37:50 +0900

    update

--- init-and-provision.sh
+++ init-and-provision.sh
@@ -10,10 +10,10 @@ fi
 if ! command -v brew &> /dev/null
 then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-  eval "$(/opt/homebrew/bin/brew shellenv)"
 else
   echo "skip: homebrew is already installed"
 fi
+eval "$(/opt/homebrew/bin/brew shellenv)"
 
 if ! command -v deno &> /dev/null
 then
